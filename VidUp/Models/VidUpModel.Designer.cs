72e8e936588fcba6cb6f7a2dc8da6eda778130ee
DevMAHE <61230012+DevMAHE@users.noreply.github.com>
2020-02-19 19:21:30 +0100

    Databse Setup

--- VidUp/Models/VidUpModel.Designer.cs
+++ VidUp/Models/VidUpModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\SourceControl\Git\VidUp\VidUp\ConsoleApp2\VidUpModel.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\SourceControl\Git\VidUp\VidUp\VidUp\Models\VidUpModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
